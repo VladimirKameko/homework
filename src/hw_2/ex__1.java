efdb67a01a009c1d87d8b643b0a1a9cdf33c1b00
VladimirKameko <drugs1360@gmail.com>
2019-05-02 11:24:26 +0300

    first commit

--- src/hw_2/ex__1.java
+++ src/hw_2/ex__1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class ex__1 {
     public static void main(String[] args) {
-        int sum=0;
+        int sum = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите число");
         int a = scan.nextInt();
-        while(a>0){
-            sum+= a%10;
-            a/=10;
+        while (a > 0) {
+            sum += a % 10;
+            a /= 10;
         }
         System.out.println(sum);
     }
