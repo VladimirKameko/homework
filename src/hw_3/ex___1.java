5770bc7b3bbff5091c6bac17d6943a08d1bf2219
VladimirKameko <drugs1360@gmail.com>
2019-05-02 12:50:49 +0300

    first commit

---
+++ src/hw_3/ex___1.java
@@ -0,0 +1,22 @@
+package hw_3;
+
+import java.util.Arrays;
+
+public class ex___1 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int positions = 1;
+        int size = array.length;
+
+        for (int i = 0; i < positions; i++) {
+            int temp = array[size - 1];
+
+            for (int j = size - 1; j > 0; j--) {
+                array[j] = array[j - 1];
+            }
+
+            array[0] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
