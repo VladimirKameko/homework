b7494c038f79ae96cd5a5679a794c152d5b8ae7c
VladimirKameko <drugs1360@gmail.com>
2019-05-02 11:47:09 +0300

    first commit

---
+++ src/hw_2/ex__2.java
@@ -0,0 +1,21 @@
+package hw_2;
+
+import java.util.Scanner;
+
+public class ex__2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int lim = scan.nextInt();
+        int n0 = 1, n1 = 1, n2;
+        System.out.print(n0 + " " + n1 + " ");
+        for (int i = 3; i < lim; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+        System.out.println();
+
+    }
+}
