055bb798f73ad069789dcf6743456fc42eab0916
VladimirKameko <drugs1360@gmail.com>
2019-05-11 00:41:26 +0300

    first commit

--- src/hw_4/Ex____1.java
+++ src/hw_4/Ex____1.java
@@ -1,7 +1,27 @@
 package hw_4;
 
-public class Ex____1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+
+public class Ex____1 {
+    private static boolean findPolindrom(String str) {
+        int n = str.length();
+        boolean a=false;
+        for (int i = 0; i < (n / 2); i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                a = false;
+            } else {
+                a = true;
+                break;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String str = scan.nextLine();
+        System.out.println(findPolindrom(str));
     }
 }
