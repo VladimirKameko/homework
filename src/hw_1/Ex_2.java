475c1569912fc0ececb0121ad5a9ba161aa40847
VladimirKameko <drugs1360@gmail.com>
2019-05-10 21:30:01 +0300

    second commit

--- src/hw_1/Ex_2.java
+++ src/hw_1/Ex_2.java
@@ -6,24 +6,38 @@ public class Ex_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите размери первого дома");
-        double p = scan.nextDouble();
-        double q = scan.nextDouble();
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
         System.out.println("Введите размери второго дома");
-        double r = scan.nextDouble();
-        double s = scan.nextDouble();
+        double c = scan.nextDouble();
+        double d = scan.nextDouble();
         double e = 20, f = 30;
-        if ((e >= p && e >= r && f >= (q + s)) ||
-                (e >= q && e >= r && f >= (p + s)) ||
-                (e >= p && e >= s && f >= (q + r)) ||
-                (e >= q && e >= s && f >= (p + r)) ||
-                (f >= p && f >= r && e >= (q + s)) ||
-                (f >= q && f >= r && e >= (p + s)) ||
-                (f >= p && f >= s && e >= (q + r)) ||
-                (f >= q && f >= s && e >= (p + r))) {
-            System.out.println("Здания помещаются");
+        checkBildings(a,b,c,d,e,f);
+    }
 
-        } else
+   private static void checkBildings(double a, double b, double c, double d, double e, double f) {
+        if (diag(a, b, c, d, e, f)) {
+            System.out.println("Здания помещаются");
+        } else if (((a + c) <= e) && (b <= f) && (d <= f)) {
+            System.out.println("Здания помещаются");
+        } else if (((b + d) <= f) && (a <= e) && (c <= e)) {
+            System.out.println("Здания помещаются");
+        } else {
             System.out.println("здания не помещаются");
+        }
+
+    }
+
+    private static boolean diag(double a, double b, double c, double d, double e, double f) {
+        boolean u = false;
+        double diag1 = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        double diag2 = Math.sqrt(Math.pow(c, 2) + Math.pow(d, 2));
+        double diag3 = Math.sqrt(Math.pow(e, 2) + Math.pow(f, 2));
+        if ((diag1 + diag2) <= diag3) {
+            u = true;
+        }
+        return u;
+
     }
 }
 
