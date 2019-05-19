415f4101867170df53930979411935b1453b3564
VladimirKameko <drugs1360@gmail.com>
2019-05-19 16:29:20 +0300

    first commit

---
+++ src/oophw1Ex1/Point.java
@@ -0,0 +1,52 @@
+package oophw1Ex1;
+
+
+
+public  final class  Point {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Point(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    public void setX(double x) {
+        if (x>=0)
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        if(x>=0)
+        this.y = y;
+    }
+    public void distance(Point point){
+        double dis= Math.sqrt((Math.pow(point.getX()-getX(),2))+((Math.pow(point.getY()-getY(),2))));
+        System.out.println(dis);
+    }
+    public void translate(double x, double y){
+        setX(getX()+x);
+        setY(getY()+y);
+
+    }
+    public void scale(double sc){
+        setX(getX()*sc);
+        setY(getY()*sc);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ src/oophw1Ex1/TestPoint.java
@@ -0,0 +1,17 @@
+package oophw1Ex1;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point p1 =new Point(8,2);
+        Point p2 =new Point(12,14);
+        p1.distance(p2);
+        Point p3 =new Point(3,4);
+        p3.translate(1,3);
+        p3.scale(2);
+        System.out.println(p3);
+
+
+    }
+
+
+}
