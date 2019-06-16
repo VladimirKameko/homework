340a53f2e33e930382fa98bb4350d40f51a3eff4
VladimirKameko <drugs1360@gmail.com>
2019-06-16 16:42:26 +0300

    final

---
+++ src/IO/ReaderWriter.java
@@ -0,0 +1,35 @@
+package IO;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ReaderWriter {
+    Map<Character, Integer> list = new TreeMap<>();
+
+    public Map<Character, Integer> readAndWrite(File inFile, File outFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inFile)))) {
+            String text;
+            while ((text = reader.readLine()) != null) {
+                for (char ch : text.replaceAll("\\s+", "").toLowerCase().toCharArray()) {
+                    if (list.get(ch) == null) {
+                        list.put(ch, 1);
+                    } else {
+                        list.put(ch, list.get(ch) + 1);
+                    }
+                }
+                text.toCharArray();
+            }
+
+
+        }
+        try(BufferedWriter write = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile)))){
+            for(Map.Entry<Character,Integer> out:list.entrySet()){
+                write.write(out.getKey()+"-"+out.getValue()+"\n");
+            }
+        }
+        return list;
+    }
+}
---
+++ src/IO/TestReaderWriter.java
@@ -0,0 +1,10 @@
+package IO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestReaderWriter {
+    public static void main(String[] args) throws IOException {
+        System.out.println(new ReaderWriter().readAndWrite(new File("C:\\gitproject\\homework\\src\\IO","\\in.txt"), new File("C:\\gitproject\\homework\\src\\IO","\\out.txt")));
+    }
+}
